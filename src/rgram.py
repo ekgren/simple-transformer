47ff08238ad5aeb62d7bf5dba1d31ecc89754882
Ariel Ekgren <ariel.ekgren@gmail.com>
2022-07-20 18:00:12 +0200

    fix

--- src/rgram.py
+++ src/rgram.py
@@ -27,13 +27,11 @@ class QuickGELU(nn.Module):
 class RMSNorm(nn.Module):
     def __init__(self, dim, eps = 1e-8):
         super().__init__()
-        self.scale = dim ** -0.5
         self.eps = eps
-        self.g = nn.Parameter(torch.ones(dim))
 
     def forward(self, x):
-        norm = torch.norm(x, dim = -1, keepdim = True) * self.scale
-        return x / norm.clamp(min = self.eps) * self.g
+        norm = torch.norm(x, dim=-1, keepdim=True)
+        return x / norm.clamp(min=self.eps)
 
 
 class ResidualBlock(nn.Module):
