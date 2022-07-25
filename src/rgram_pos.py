851e30e87246c4fc28313744bb34458a71176b16
Ariel Ekgren <ariel.ekgren@gmail.com>
2022-07-26 00:00:19 +0200

    stupid test

--- src/rgram_pos.py
+++ src/rgram_pos.py
@@ -67,12 +67,12 @@ class MergeBlocks(nn.Module):
         super().__init__()
         self.mergeblocks = nn.ModuleList([MergeBlock(config, level=i) for i in range(config.n_layer)])
         self.lns = nn.ModuleList([LayerNorm(config.n_embd) for _ in range(config.n_layer)])
-        self.vq = VectorQuantize(dim=config.n_embd, codebook_size=config.vocab_size, decay=0.8, commitment_weight=1.)
+        #self.vq = VectorQuantize(dim=config.n_embd, codebook_size=config.vocab_size, decay=0.8, commitment_weight=1.)
 
     def forward(self, input: torch.Tensor, seq_ids: Optional[torch.Tensor] = None) -> torch.Tensor:
         for ln, mergeblock in zip(self.lns, self.mergeblocks):
-            quantized, indices, commit_loss = self.vq(input)  # (1, 1024, 256), (1, 1024), (1)
-            input = ln(mergeblock(input, seq_ids) + quantized)  # merge -> residual -> layer norm
+            #quantized, indices, commit_loss = self.vq(input)  # (1, 1024, 256), (1, 1024), (1)
+            input = ln(mergeblock(input, seq_ids) + input)  # merge -> residual -> layer norm
             return input
 
 
