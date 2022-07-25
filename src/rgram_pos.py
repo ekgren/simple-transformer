a1aba746dd033e96f656c0a0d98254d785362a37
Ariel Ekgren <ariel.ekgren@gmail.com>
2022-07-25 23:32:11 +0200

    stupid test

--- src/rgram_pos.py
+++ src/rgram_pos.py
@@ -239,27 +239,25 @@ class RgramPos(nn.Module):
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
         idx = idx.view(-1)
-        for _ in range(max_new_tokens):
-            # if the sequence context is growing too long we must crop it at block_size
-            #idx_cond = idx if idx.size(1) <= self.block_size else idx[:, -self.block_size:]
-            # forward the model to get the logits for the index in the sequence
-            seq_ids = idx.new_ones(idx.shape)
-            logits, _ = self(torch.stack((idx, seq_ids)))
-            # pluck the logits at the final step and scale by desired temperature
-            logits = logits[-1, :] / temperature
-            # optionally crop the logits to only the top k options
-            if top_k is not None:
-                v, _ = torch.topk(logits, top_k)
-                logits[logits < v[[-1]]] = -float('Inf')
-            # apply softmax to convert logits to (normalized) probabilities
-            probs = F.softmax(logits, dim=-1)
-            # either sample from the distribution or take the most likely element
-            if do_sample:
-                idx_next = torch.multinomial(probs, num_samples=1)
-            else:
-                _, idx_next = torch.topk(probs, k=1, dim=-1)
-            # append sampled index to the running sequence and continue
-            idx = torch.cat((idx, idx_next), dim=-1)
+        # if the sequence context is growing too long we must crop it at block_size
+        #idx_cond = idx if idx.size(1) <= self.block_size else idx[:, -self.block_size:]
+        # forward the model to get the logits for the index in the sequence
+        logits, _ = self(idx)
+        # pluck the logits at the final step and scale by desired temperature
+        logits = logits / temperature
+        # optionally crop the logits to only the top k options
+        if top_k is not None:
+            v, _ = torch.topk(logits, top_k)
+            logits[:, logits < v[[-1]]] = -float('Inf')
+        # apply softmax to convert logits to (normalized) probabilities
+        probs = F.softmax(logits, dim=-1)
+        # either sample from the distribution or take the most likely element
+        if do_sample:
+            idx = torch.multinomial(probs, num_samples=1)
+        else:
+            _, idx = torch.topk(probs, k=1, dim=-1)
+        # append sampled index to the running sequence and continue
+        #idx = torch.cat((idx, idx_next), dim=-1)
 
         return idx.unsqueeze(0)
 
