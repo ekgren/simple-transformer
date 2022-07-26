5933c81b7893627793557e65ddf5fcc387c1fc69
Ariel Ekgren <ariel.ekgren@gmail.com>
2022-07-26 13:30:45 +0200

    fixing forward NSP

--- src/rgram_pos.py
+++ src/rgram_pos.py
@@ -111,7 +111,7 @@ class NSP(nn.Module):
         device = idx.device
         tok_emb = self.wte(idx)  # token embeddings of shape (b * t, n_embd)
         pos_emb = self.wpe(pos_ids)  # position embeddings of shape (b * t, n_pos_embd)
-        x = pos_emb + torch.where(idx.view(-1, 1) > -1, pos_emb, tok_emb)
+        x = pos_emb + torch.where(idx.view(-1, 1) > -1, tok_emb, pos_emb)
         x = self.ln_e(x)
         x = self.drop(x)
         loss = None
