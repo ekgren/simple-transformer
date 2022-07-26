e8937728e53c7a1ceba53fe0e5d8955b7c9b06e7
Ariel Ekgren <ariel.ekgren@gmail.com>
2022-07-26 13:34:45 +0200

    fixing forward NSP

--- src/rgram_pos.py
+++ src/rgram_pos.py
@@ -122,6 +122,7 @@ class NSP(nn.Module):
             logits = self.lm_head(x)
 
             # Sample new tokens
+            print(logits)
             probs = F.softmax(logits, dim=-1)
             print(probs)
             idx = torch.multinomial(probs, num_samples=1).view(-1)
